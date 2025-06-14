bcf694b094d372d0ca732711171d985a86db2232
杨再涛 <1772081518@qq.com>
2025-06-14 18:06:23 +0800

    完成二维码生成接口，添加结果查询接口70%

---
+++ uniCloud-aliyun/cloudfunctions/pay_query/config.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const pid = "2025040423043232";
+const key = "f8848mCKqEGc51N5Fp69FZNyNQbtFPqp";
+const query_url = "https://zpayz.cn/api.php";
+exports.premium_check = async function(device_id, out_trade_no) {
+	const url = query_url.concat(`?act=order&pid=${pid}&key=${key}&out_trade_no=${out_trade_no}`);
+	const res = await axios.get(url);
+	
+	if (res.status == 200) {
+		const db = uniCloud.database().collection('hkhj3_premium');
+		const query_data = await db.where({
+			"device_id": device_id
+		}).get();
+		//创建订阅数据
+		if (query_data.affectedDocs == 0) {
+			
+			await db.add({
+				'device_id': device_id,
+				'premium_time': res['data']['endtime']
+			})
+			//更新订阅数据
+		} else if (query_data.affectedDocs == 1) {
+			db.doc(query_data['data'][0]['_id']).update({
+				'premium_time': ''
+			});
+		}
+		return {
+			code: 200,
+			message: "订阅成功！"
+		}
+	}
+	return {
+		code: 500,
+		data: res['data'],
+		message: "服务器内部错误！"
+	};
+}
---
+++ uniCloud-aliyun/cloudfunctions/pay_query/index.js
@@ -0,0 +1,15 @@
+const {
+	premium_check
+} = require('./config.js');
+'use strict';
+exports.main = async (event, context) => {
+	//event为客户端上传的参数
+	console.log('event : ', event)
+	//返回数据给客户端
+	const body = JSON.parse(event['body']);
+	const device_id = body['device_id'];
+	const out_trade_no = body['out_trade_no'];
+	const data = await premium_check(device_id, out_trade_no);
+
+	return data;
+};
